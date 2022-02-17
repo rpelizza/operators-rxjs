743fc66e55118da5b4898c70232313e64ca7c692
Rafael Pelizza <rafael.pelizza@gmail.com>
2022-02-17 14:46:11 -0300

    refactor: remove behavior subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
 import { IRxjsList } from './interfaces/IRxjsList.interface';
 import { AppSettingsService } from './services/app-settings.service';
 
@@ -9,29 +8,26 @@ import { AppSettingsService } from './services/app-settings.service';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 	public title: string = 'operators-rxjs';
+	public items: IRxjsList[] = [];
 	public defaultSelected: string = '';
 
-	public RxjsItems$ = new BehaviorSubject<IRxjsList[] | null>(null);
-
-	constructor(private router: Router, private appSettingsService: AppSettingsService) {}
+	constructor(
+		private router: Router,
+		private appSettingsService: AppSettingsService
+	) {}
 
 	ngOnInit(): void {
-		this.appSettingsService
-			.getLocalJson()
-			.subscribe((x) => this.RxjsItems$.next(x));
-
-		this.RxjsItems$.subscribe((val) => {
-			val && (this.defaultSelected = val[0].route);
+		this.appSettingsService.getLocalJson().subscribe({
+			next: (data) => {
+				this.items = data;
+				this.defaultSelected = data[0].route;
+			},
 		});
 	}
 
 	selectRXJS(event: Event) {
 		this.router.navigate([event]);
-	}
-
-	ngOnDestroy(): void {
-		this.RxjsItems$.unsubscribe();
 	}
 }
